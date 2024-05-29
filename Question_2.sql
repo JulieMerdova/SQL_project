d0ad1cd6bab48e7561b2e8c267fec553d53334be
Julie Merdova <julie.talpova@gmail.com>
2024-05-29 11:40:23 +0200

    Q2 + Q3

---
+++ Question_2.sql
@@ -0,0 +1,35 @@
+-- JULIE MERDOVA
+
+/* 2. Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední 
+srovnatelné období v dostupných datech cen a mezd?
+*/
+
+-- nejprve zjistíme počátek a konec dostupných měření pro mléko a chleba
+
+SELECT food_category,
+		min (actual_yr),
+		max (actual_yr)
+FROM t_julie_merdova_project_sql_primary_final tjm
+WHERE 	category_code IN ( 111301, 114201)
+GROUP BY food_category
+;
+
+-- pro naše účely budeme tedy srovnávat roky 2006 a 2018, nyní zjistíme průměrné ceny v těchto obdobích:
+
+SELECT 
+	payroll_year,
+	food_category,
+	actual_yr,
+	avg_price ,
+	average_wage ,
+	round (average_wage/ avg_price, 0) AS avg_amount
+FROM t_julie_merdova_project_sql_primary_final tjm  
+WHERE	payroll_year IN (2006, 2018) 
+		AND category_code IN ( 111301, 114201)
+		AND actual_yr IN (2006, 2018)
+GROUP BY 	payroll_year, 
+			food_category, 	
+			actual_yr
+;
+-- v roce 2006 jsme si za průměrnou mzdu 14 818 Kč mohli koupit 919 kg chleba a 1026 litrů mléka.
+-- V roce 2018 jsme si za průměrnou mzdu 25 467 Kč mohli koupit 1051 kg chleba a 1285 litrů mléka.
---
+++ Question_3.sql
@@ -0,0 +1,18 @@
+-- JULIE MERDOVA
+
+/* 3. Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
+ */
+
+WITH growth_prices AS (
+		SELECT 	food_category,
+				avg_price,
+				LAG(avg_price) OVER (PARTITION BY food_category ORDER BY actual_yr) AS prev_avg_price
+		FROM t_julie_merdova_project_sql_primary_final tjm
+				)
+	SELECT	food_category,
+			ROUND(AVG((avg_price - prev_avg_price)/prev_avg_price),6)*100 AS price_growth_AVG
+	FROM growth_prices
+		WHERE prev_avg_price IS NOT NULL 
+	GROUP BY food_category
+	ORDER BY price_growth_AVG
+;
