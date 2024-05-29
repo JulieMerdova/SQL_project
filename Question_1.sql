57e12b1ccd6b0ce02eee90a1e9431ff186098cb8
JulieMerdova <160959450+JulieMerdova@users.noreply.github.com>
2024-05-29 12:00:12 +0200

    Update Question_1.sql

--- Question_1.sql
+++ Question_1.sql
@@ -4,21 +4,21 @@
 
 WITH wage_grow AS (
 	SELECT	industry_branch_code,
-			payroll_year,
-			average_wage ,
-			lag(average_wage) OVER (PARTITION BY industry_branch_code ORDER BY payroll_year) AS prev_wage
+		payroll_year,
+		average_wage ,
+		lag(average_wage) OVER (PARTITION BY industry_branch_code ORDER BY payroll_year) AS prev_wage
 	FROM t_julie_merdova_project_sql_primary_final tjm
 	GROUP BY industry_branch_code,
-			average_wage  
+		average_wage  
 	)
 	SELECT	industry_branch_code,
-			average_wage,
-			prev_wage,
-			payroll_year,
-			(SELECT payroll_year - 1) AS prev_payroll_yr,
-			avg((average_wage-prev_wage)/prev_wage)*100 AS avg_grow_perc
+		average_wage,
+		prev_wage,
+		payroll_year,
+		(SELECT payroll_year - 1) AS prev_payroll_yr,
+		avg((average_wage-prev_wage)/prev_wage)*100 AS avg_grow_perc
 	FROM 	wage_grow
 	WHERE payroll_year > 2006
 	GROUP BY	industry_branch_code, 
-				payroll_year
+			payroll_year
 ;
