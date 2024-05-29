0b39ea1f5309d6b1001070f51440df981ef1a204
JulieMerdova <160959450+JulieMerdova@users.noreply.github.com>
2024-05-29 12:02:19 +0200

    Update Question_3.sql

--- Question_3.sql
+++ Question_3.sql
@@ -5,14 +5,14 @@
 
 WITH growth_prices AS (
 		SELECT 	food_category,
-				avg_price,
-				LAG(avg_price) OVER (PARTITION BY food_category ORDER BY actual_yr) AS prev_avg_price
+			avg_price,
+			LAG(avg_price) OVER (PARTITION BY food_category ORDER BY actual_yr) AS prev_avg_price
 		FROM t_julie_merdova_project_sql_primary_final tjm
 				)
 	SELECT	food_category,
-			ROUND(AVG((avg_price - prev_avg_price)/prev_avg_price),6)*100 AS price_growth_AVG
+		ROUND(AVG((avg_price - prev_avg_price)/prev_avg_price),6)*100 AS price_growth_AVG
 	FROM growth_prices
-		WHERE prev_avg_price IS NOT NULL 
+	WHERE prev_avg_price IS NOT NULL 
 	GROUP BY food_category
 	ORDER BY price_growth_AVG
 ;
